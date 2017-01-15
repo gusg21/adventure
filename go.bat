f30c9c4a05aee415b7e81d4a82440e3db271133f
gusg21 <gusg21@parkschool.net>
2017-01-15 11:37:20 -0500

    Easier to understand; and also added options.bat

--- go.bat
+++ go.bat
@@ -1,17 +1,20 @@
 @echo off
+call options.bat
 cls
 type start-text.txt
 pause>nul
 cls
 cd main
 :A
+echo Options:
 dir /ad /b
 echo.
+echo Description:
 if EXIST text.txt ( type text.txt )
 if EXIST script.bat ( call script.bat )
 echo.
 echo.
-set /p input=Option: 
+set /p input=Choice: 
 if EXIST %input% ( cd %input% ) else ( echo Unknown Option! && pause>nul )
 cls
 goto A
---
+++ options.bat
@@ -0,0 +1,7 @@
+:: Change the code below to configure your game!
+
+:: The title of the window
+title Adventure
+
+:: The color of the window (See: http://ss64.com/nt/color.html)
+color 9f
