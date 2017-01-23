0230ea19b6d054b3053f0be9835b7bdd5eb9b59d
Gus G <gusg21@parkschool.net>
2017-01-23 18:32:56 -0500

    Added documentation

--- go.bat
+++ go.bat
@@ -1,19 +1,37 @@
+	REM adventure Game Engine
+
+	REM by gusg21
+
+	REM (c) 2017
+	
+	REM ==================================
+
+	REM The greatest batch game engine ever. *ahem* One of the only batch game engines ever.
+	REM It's pretty great. In my completely un-humble opinion.
+
+	REM See how-to.txt to get going!
+
 @echo off
+REM Load options
 call options.bat
 cls
+REM Display start message
 type start-text.txt
 pause>nul
 cls
 cd main
 :A
+REM Show directories as options
 echo Options:
 dir /ad /b
 echo.
+REM Show text.txt as the description
 echo Description:
 if EXIST text.txt ( type text.txt )
 if EXIST script.bat ( call script.bat )
 echo.
 echo.
+REM Detect "undo" command
 for %%* in (.) do set cdname=%%~nx*
 set /p input=Choice: 
 if %canUndo% EQU 1 ( 
@@ -23,6 +41,7 @@ cd ..
 )
 )
 )
+REM Move into the option chosen
 if EXIST "%input%" ( cd "%input%" ) else ( 
 if "%input%" EQU "undo" (
 echo Undoing... && pause>nul
