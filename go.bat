fab2df80eaa5a74432c7955c3b8601bd4abcfc80
Gus G <gusg21@parkschool.net>
2017-01-23 17:58:36 -0500

    Fully implemented undo, with prevention of leaving
    You can type "undo" (if enabled in options.bat) to go back to the last scenario. Also you can't leave the main folder using that, which would turn this into a filesystem viewer.

--- go.bat
+++ go.bat
@@ -14,7 +14,21 @@ if EXIST text.txt ( type text.txt )
 if EXIST script.bat ( call script.bat )
 echo.
 echo.
+for %%* in (.) do set cdname=%%~nx*
 set /p input=Choice: 
-if EXIST "%input%" ( cd "%input%" ) else ( echo Unknown Option! && pause>nul )
+if %canUndo% EQU 1 ( 
+if "%input%" EQU "undo" ( 
+if "%cdname%" NEQ "main" (
+cd .. 
+)
+)
+)
+if EXIST "%input%" ( cd "%input%" ) else ( 
+if "%input%" EQU "undo" (
+echo Undoing... && pause>nul
+) else (
+echo Unknown Option! && pause>nul 
+)
+)
 cls
 goto A
